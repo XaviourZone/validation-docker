d4e2925f30ee3657402e81e4b33015a7660273e4
XaviourZone <yasxanto@gmail.com>
2026-09-18 21:24:44 +0530

    Port Validation/Data_Router/app/routing/__init__.py from validation baseline

---
+++ Validation/Data_Router/app/routing/__init__.py
@@ -0,0 +1,7 @@
+"""Routing data structures and engine."""
+
+from .destination import ParserDestination
+from .route import Route
+from .router import RoutingEngine
+
+__all__ = ["ParserDestination", "Route", "RoutingEngine"]

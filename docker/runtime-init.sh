649041ca9c789c0459a9de59835a34dcad4dbb43
XaviourZone <yasxanto@gmail.com>
2026-09-19 00:01:39 +0530

    Add offline deployment support: docker/runtime-init.sh

---
+++ docker/runtime-init.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -eu
+
+mkdir -p \
+  runtime/data_inflow/SAIS_IOR \
+  runtime/data_inflow/SAIS_GLOBAL \
+  runtime/data_inflow/MSIS \
+  runtime/data_inflow/LRIT \
+  runtime/spool/pending \
+  runtime/spool/delivered \
+  runtime/spool/failed \
+  runtime/router-state \
+  runtime/router-logs \
+  runtime/parser-logs \
+  runtime/reference
+
+printf '%s\n' "Runtime directories initialized."

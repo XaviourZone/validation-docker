bd83664045e0878453c9b5bec98b953c97e8bf9b
YASSIN XAVIOUR A <yasxanto@gmail.com>
2026-09-23 19:14:38 +0530

    Make Windows DB startup launch manager and local console

--- Validation/DB_Manager/START_DB.bat
+++ Validation/DB_Manager/START_DB.bat
@@ -8,4 +8,6 @@ if not "%VALIDATION_PG_CTL%"=="" if not "%VALIDATION_PG_DATA%"=="" (
     "%VALIDATION_PG_CTL%" -D "%VALIDATION_PG_DATA%" -l "%CD%\logs\postgresql.log" start
   )
 )
-python Validation\DB_Manager\db_manager.py
+start "" python Validation\DB_Manager\db_manager.py
+timeout /t 3 /nobreak >nul
+start "" http://127.0.0.1:5050
